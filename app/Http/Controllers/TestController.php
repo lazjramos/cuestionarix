3e7e21c2f05988e033ba262afbc6272033aaa71f
Laz J. Ramos <lazjramos@gmail.com>
2017-07-12 13:30:06 -0500

    Extracting data from DDBB to view

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -3,10 +3,17 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Question;
+use App\Answer;
 
 class TestController extends Controller
 {
     public function index(){
-        return view('index');
+        $questions = Question::all();
+        $answers = Answer::all();
+
+        return view('index')
+            ->with('questions',$questions)
+            ->with('answers', $answers);
     }
 }
