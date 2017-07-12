6a1036e5f8d5ead4f19cd50824cfc096c91c8408
Laz J. Ramos <lazjramos@gmail.com>
2017-07-12 10:52:25 -0500

    Changin permissions

---
+++ resources/views/index.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <title>Cuestionarix test</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+    <link href="css/style.css" rel="stylesheet">
+</head>
+
+<body>
+    <h1>Hola mundo</h1>
+</body>
+
+</html>
